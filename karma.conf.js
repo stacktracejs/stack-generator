826bfeb50dfcc0f2e866fd6559eb4abb8d86b64f
Eric Wendelin <eric@gradle.com>
2022-06-05 14:45:01 -0700

    Fix code coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -9,18 +9,28 @@ module.exports = function (config) {
             'spec/*-spec.js',
             { pattern: 'spec/fixtures/**', included: false }
         ],
-        reporters: ['spec'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         customLaunchers: {
-            Chrome_Travis: {
+            Chrome_No_Sandbox: {
                 base: 'Chrome',
                 flags: ['--no-sandbox']
             }
         },
         browsers: ['PhantomJS'],
+        reporters: ['spec', 'saucelabs', 'coverage', 'coveralls'],
+        preprocessors: {
+            'stack-generator.js': 'coverage'
+        },
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage',
+            subdir: function(browser) {
+                return browser.toLowerCase().split(/[ /-]/)[0];
+            }
+        },
         singleRun: false
     });
 };
